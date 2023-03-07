767e5bc34f6711b0a12e2bd3cc04158b7e8ae0e8
nasirisreal <nasirisreal@yahoo.com>
2023-03-07 12:22:52 -0800

    Adding a Python function that prints Fizz, Buzz and FizzBuzz for multiples of three, five and both

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-     for number in range(1, 101):
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
         elif number % 3 == 0:
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif number % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{}".format(number), end="")
+            print("{} ".format(number), end="")
