13b5ec1a2cf7dfdb4bea8b160ed708dbc7a33f99
nasirisreal <nasirisreal@yahoo.com>
2023-03-07 11:17:08 -0800

    Adding a Python script that prints all the alphabets in lower case except e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) is not '101' and chr(letter) is not '113':
+    if chr(letter) is not 'e' and chr(letter) is not 'q':
         print("{}".format(chr(letter)), end="")
