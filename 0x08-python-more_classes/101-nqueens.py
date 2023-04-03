c7cad1b0330dcb85e361019359bc708f4b746e90
nasirisreal <nasirisreal@yahoo.com>
2023-04-03 07:24:54 -0700

    N Queens puzzle

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
 """
+
 This module contains an algorithm that resolves the N-Queen puzzle
 using backtracking
-"""
 
+"""
 
 def isSafe(m_queen, nqueen):
     """ Method that determines if the queens can or can't kill each other
@@ -16,12 +16,17 @@ def isSafe(m_queen, nqueen):
     Returns:
         True: when queens can't kill each other
         False: when some of the queens can kill
+
     """
+
     for i in range(nqueen):
+
         if m_queen[i] == m_queen[nqueen]:
             return False
+
         if abs(m_queen[i] - m_queen[nqueen]) == abs(i - nqueen):
             return False
+
     return True
 
 
@@ -31,8 +36,14 @@ def print_result(m_queen, nqueen):
     Args:
         m_queen: array that has the queens positions
         nqueen: queen number
+
     """
-    res = [(i, m_queen[i]) for i in range(nqueen)]
+
+    res = []
+
+    for i in range(nqueen):
+        res.append([i, m_queen[i]])
+
     print(res)
 
 
@@ -42,14 +53,16 @@ def Queen(m_queen, nqueen):
     Args:
         m_queen: array that has the queens positions
         nqueen: queen number
+
     """
+
     if nqueen == len(m_queen):
         print_result(m_queen, nqueen)
         return
 
-    m_queen[nqueen] = -1
+    m_queen[nqueen] = 0
 
-    while m_queen[nqueen] < len(m_queen) - 1:
+    while(m_queen[nqueen] < len(m_queen)):
 
         m_queen[nqueen] += 1
 
@@ -59,20 +72,24 @@ def Queen(m_queen, nqueen):
                 Queen(m_queen, nqueen + 1)
 
 
-def solveNQueens(size):
+def solveNQueen(size):
     """ Function that invokes the Backtracking algorithm
 
     Args:
         size: size of the chessboard
+
     """
-    m_queen = [-1 for i in range(size)]
+
+    m_queen = [0 for i in range(size)]
+
     Queen(m_queen, 0)
 
 
 if __name__ == '__main__':
+
     import sys
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
@@ -86,5 +103,5 @@ if __name__ == '__main__':
         print("N must be at least 4")
         sys.exit(1)
 
-    solveNQueens(size)
+    solveNQueen(size)
 
