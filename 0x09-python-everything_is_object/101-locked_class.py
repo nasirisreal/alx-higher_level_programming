520f587fd6c243283d0849a332a8a3bd934724dd
nasirisreal <nasirisreal@yahoo.com>
2023-04-04 07:19:22 -0700

    Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
-"""
-
-This is a module that containts a clas that avoids
-dynmaically created attributes
-
-"""
-
-
 class LockedClass:
-    __slots__ = ['first_name']
-
-    def __init__(first_name):
-        """
-        Initialize an instance of LockedClass with a first name.
-        :param first_name: The first name of the instance.
-        """
-        pass
+    def __setattr__(self, key, value):
+        if key != 'first_name':
+            raise AttributeError(f"{self.__class__.__name__} object has no attribute '{key}'")
+        self.__dict__[key] = value
