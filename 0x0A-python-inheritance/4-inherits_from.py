b4db9c3a49c96421fb73b4504a2ad15a59a3db11
nasirisreal <nasirisreal@yahoo.com>
2023-04-11 05:34:56 -0700

    Python Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
+
 def inherits_from(obj, a_class):
-    """ Function that returns True/False if obj is an instance of a_class
+    """Function that returns True/False if obj is an instance of a_class
 
     Args:
         obj (any): The object to check.
