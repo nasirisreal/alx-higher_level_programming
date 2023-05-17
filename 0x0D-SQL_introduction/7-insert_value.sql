6588bf4e32665670fed37092dc92edae7fa41336
nasirisreal <nasirisreal@yahoo.com>
2023-05-16 21:46:45 -0700

    Update

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- Inserts a new row in a table
--- Query to insert a new row in the table first_table
+-- Inserts a new row in the first_table in the database used
+-- Query to perform operation
 INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
