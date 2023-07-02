ec94ef5e2f63ce1337d1de77e311ca61eb788b82
nasirisreal <nasirisreal@yahoo.com>
2023-07-02 09:33:04 -0700

    Adding a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A Python script that that takes in a URL and an email, sends a POST request to a given URL with a given email.
+"""Sends a POST request to a given URL with a given email.
 
 Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
