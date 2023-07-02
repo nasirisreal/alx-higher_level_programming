79d4ca12390e26c1e86769b91a4a797ed86274ca
nasirisreal <nasirisreal@yahoo.com>
2023-07-02 07:51:57 -0700

    Adding a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     request = urllib.request.Request(url)
     
     with urllib.request.urlopen(request) as response:
-        body = response.read().decode("utf-8")
+        body = response.read()
         
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
