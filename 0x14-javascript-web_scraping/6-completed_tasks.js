bf4f4e34ce609aaddada92e828ac49d9b842b0a4
nasirisreal <nasirisreal@yahoo.com>
2023-07-25 21:38:37 +0000

    Adding a scripts that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
+const argv = process.argv;
+const url = argv[2];
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const rbody = JSON.parse(body);
+    const dict = {};
+    for (const i of rbody) {
+      if (i.completed === true) {
+        if (dict[i.userId] === undefined) {
+          dict[i.userId] = 0;
+        }
+        dict[i.userId] += 1;
       }
-    });
-    console.log(completed);
+    }
+    console.log(dict);
   }
 });
